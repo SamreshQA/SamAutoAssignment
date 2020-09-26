bba124bdb9348e3bc00466494312406b4f5d76fe
Samresh Bhardwaj <Samresh.bhardwaj@Gmail.com>
2020-09-26 18:28:28 +1000

    bdd-setup
    bdd-setup

--- cypress/integration/sample_test.js
+++
@@ -1,6 +0,0 @@
-describe('My First Cypress Test', function() {
-    it('Visits the ToolsQA Demo Page and check the menu items', function() {
-    //Visit the Demo QA Website
-    cy.visit(url);
-})
-})
